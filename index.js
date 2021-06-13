e168d320ca907bf205fee2ec920a97d3ef8fb793
anoopaneesh <anoopaneeh808@gmail.com>
2021-06-13 16:58:36 +0530

    loading added

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 function main() {
-  let btn = document.getElementById("proceed-btn");
-  console.log(btn);
-  btn.addEventListener("click", () => {
-    window.location = "./level1/level1.html";
-  });
+  let body = document.querySelector('body')
+  let btn = document.getElementById('proceed-btn')
+  let paperImg = document.getElementById('paper-img')
+  document.querySelector('html').style.setProperty('--bg','#000');
+  let content = body.innerHTML
+  body.innerHTML = `
+  <div id="load">
+  <div>G</div>
+  <div>N</div>
+  <div>I</div>
+  <div>D</div>
+  <div>A</div>
+  <div>O</div>
+  <div>L</div>
+</div>
+  `
+  loadImage(paperImg, './images/1-min.png').then((image) => {
+    console.log('Loaded image')
+    console.log(content)
+    body.innerHTML = content;
+    document.querySelector('html').style.setProperty('--bg',`url('./images/intro1-min.png')`);
+  })
+  console.log(btn)
+  btn.addEventListener('click', () => {
+    window.location = './level1/level1.html'
+  })
 }
-main();
+main()
+function loadImage(img, src) {
+  return new Promise((resolve, reject) => {
+    img.onload = () => resolve(img)
+    img.onerror = reject
+    img.src = src
+  })
+}
